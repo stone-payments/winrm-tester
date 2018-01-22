9cb2ff266be1ca1071066cd94553a87ee8219e57
Douglas Quintanilha Barbosa Ferreira | Stone <dbferreira@stone.com.br>
2018-01-22 12:04:57 -0200

    Creating -debug flag

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def inicialize_arguments_parser():
     parser.add_argument('-u', '--user', help='Username for the WinRM connection',required=True, dest='user')
     parser.add_argument('-p', '--password', help='Password for the WinRM connection',required=False, dest='password')
     parser.add_argument('-port', help='Change the WinRM connection port',required=False, default="5986", dest='port')
+    parser.add_argument('-d', '--debug' , help='Enable debug messages',required=False, default=False, dest='debug')
     args = parser.parse_args()
     
     return args
@@ -23,6 +24,7 @@ def main():
     user = args.user
     port = args.port
     password = args.password
+    debug = args.debug
 
     if password is None:
         password = getpass.getpass('Digite com o Password: ')
@@ -37,14 +39,16 @@ def main():
         command_id = protocol.run_command(shell_id, 'whoami')
         std_out, std_err, status_code = protocol.get_command_output(shell_id, command_id)
         protocol.cleanup_command(shell_id, command_id)
-        protocol.close_shell(shell_id)        
-        print(std_out)
-        print(status_code)
+        protocol.close_shell(shell_id)
+        if(debug):        
+            print(std_out)
+            print(status_code)
         return 0
 
-    except Exception:                
-        print("Error while trying to connect via WinRM")
-        print(sys.exc_info()[1])
+    except Exception:     
+        if(debug):                   
+            print("Error while trying to connect via WinRM")
+            print(sys.exc_info()[1])
         return 1
 
 
